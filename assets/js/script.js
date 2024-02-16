f95ceac81ceb95031a62dde5fd16f0ff27a14270
William Eckerleben <wgeckero@bdp.com.py>
2024-02-16 10:49:07 -0300

    restructuring and dark theme

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function toggleTheme() {
+  const currentTheme = document.body.getAttribute("data-theme");
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
+  document.body.setAttribute("data-theme", newTheme);
+}
